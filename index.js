0df803f925687e59a5835fca98dd48ce5ea6186d
OnyinyeTech <oazubike5@gmail.com>
2024-11-30 10:32:43 +0100

    m

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const User = require("./models/user.model.js");
 const taskApp = require("./routes/taskRoutes.js");
+const PORT = process.env.PORT || 5000;
 
 const dotenv = require("dotenv");
 
@@ -275,8 +276,8 @@ mongoose
   )
   .then(() => {
     console.log("Connected to database!");
-    app.listen(5000, () => {
-      console.log("Server is running on port 5000");
+    app.listen(PORT, () => {
+      console.log("Server is running on port ${PORT}");
     });
   })
   .catch(() => {
