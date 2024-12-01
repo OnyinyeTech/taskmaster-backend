288d414d3c595793e8907a3be24765df50ad8a7e
OnyinyeTech <oazubike5@gmail.com>
2024-12-01 06:25:07 +0100

    Update index.js

--- index.js
+++ index.js
@@ -282,8 +282,8 @@ mongoose
   )
   .then(() => {
     console.log("Connected to database!");
-    app.listen(5000, () => {
-      console.log("Server is running on port 5000");
+    app.listen(3000, () => {
+      console.log("Server is running on port 3000");
     });
   })
   .catch(() => {
